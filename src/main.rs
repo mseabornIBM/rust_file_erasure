5dd416efa0b773132927223e91a70dbf324a5482
mseaborn@us.ibm.com <musterjunk@gmail.com>
2022-02-27 16:30:12 -0600

    reading from a file and sharding, deleting and reconstruction working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 
 
 #[macro_use(shards)]
-#[macro_use(convert_2D_slices)]
 extern crate reed_solomon_erasure;
 
 use reed_solomon_erasure::galois_8::ReedSolomon;
@@ -9,7 +8,7 @@ use reed_solomon_erasure::galois_8::ReedSolomon;
 //use reed_solomon_erasure::galois_16::ReedSolomon;
 
 use std::io::{self, Read, Write, ErrorKind, BufReader};
-use fixed_buffer::{FixedBuf};
+//use fixed_buffer::{FixedBuf};
 use std::fs::File;
 use std::path::Path;
 
@@ -17,11 +16,11 @@ fn main() {
     code_it();
     playground();
     vec_test();
-    //read_and_code_it();
+    read_and_code_it();
 }
 
 fn read_and_code_it(){
-    let path = Path::new("image.png");
+    let path = Path::new("image.jpg");
     let display = path.display();
     let mut file = match File::open(&path) {
         Err(why) => panic!("couldn't open {}: {}", display, why),
@@ -29,30 +28,42 @@ fn read_and_code_it(){
     };
 
     const LEN: usize = 4096;
-    let mut lines:Vec<[u8; LEN]> = Vec::new();
-    let mut buffer = [0u8; LEN];
-    while file.read(&mut buffer).unwrap() > 0 {
-        lines.push(buffer);
-        buffer = [0u8; LEN];
-        //println!("{:?}", buffer);
+    //let mut lines:Vec<[u8; LEN]> = Vec::new();
+    let mut lines: std::vec::Vec<std::vec::Vec<u8>> = Vec::new();
+    let mut line_buffer = [0u8; LEN];
+    while file.read(&mut line_buffer).unwrap() > 0 {
+        let mut line_vec: Vec<u8> = Vec::new();
+
+        for _i in 0..LEN{
+            line_vec.push(line_buffer[_i]);
+        }
+
+        /*for (place, data) in line_vec.iter_mut().zip(line_buffer.iter()){
+            line_vec.push(*data);
+            *place = *data;
+        }*/
+
+        lines.push(line_vec);
+        line_buffer = [0u8; LEN];
     }
     println!("Vector size is {} lines.", lines.len());
 
+    //add parity slices
     let parity = 10;
     for _i in 0..parity {
-        lines.push([0; LEN]);
-        //println!("{}", _i);
+        let mut line_vec: Vec<u8> = Vec::new();
+        for _j in 0..LEN{
+            line_vec.push(0);
+        }
     }
 
         
     /*for _i in 0..lines.len(){
         println!("row {} = {:?}", _i, lines[_i]);
     }*/
+
     println!("Matrix is of length {}", (lines.len()));
     println!("Vector size is {} lines plus {} lines of parity.", lines.len()-parity, parity);
-
-    //const ROWS: usize = lines.len() as usize;
-    //let mut line_array =[[0u8; LEN]; ROWS];
 
     let r = ReedSolomon::new(lines.len()-parity, parity).unwrap();
     r.encode(&mut lines).unwrap();
@@ -63,15 +74,17 @@ fn read_and_code_it(){
     //remove 2 shards for reconstruction later
     shards[0] = None;
     shards[4] = None;
+    shards[15] = None;
 
     // Try to reconstruct missing shards
-    //r.reconstruct(&mut shards).unwrap();
+    r.reconstruct(&mut shards).unwrap();
 
     // Convert back to normal shard arrangement
     let result: Vec<_> = shards.into_iter().filter_map(|x| x).collect();
 
     assert!(r.verify(&result).unwrap());
     assert_eq!(lines, result);
+    println!("{}", "File reconstruction successful.")
 }
 
 fn code_it(){
@@ -153,10 +166,11 @@ fn vec_test(){
         [0, 0,  0,  0, 0]
     );
 
-
+    //start a test to dynamically allocate the slices
     const LEN: usize = 5;
     let mut lines: std::vec::Vec<std::vec::Vec<u8>> = Vec::new();
 
+    //create a row for each row in master_copy
     for _i in 0..4 {
         let mut row_vec:Vec<u8> = Vec::new();
 
@@ -197,19 +211,18 @@ fn vec_test(){
     shards[0] = None;
     shards[4] = None;
 
-    let mut _i = 0;
-    for shard in shards.iter(){
-        println!("shard {} of shards = {:?}", _i, shards[_i]);
-        _i = _i + 1;
+    //let mut _i = 0;
+    for _r in 0..shards.len(){    
+        println!("shard {} of shards = {:?}", _r, shards[_r]);
     }
 
     // Try to reconstruct missing shards
     r.reconstruct(&mut shards).unwrap();
 
-    let mut _i = 0;
-    for shard in shards.iter(){
-        println!("shard {} of shards = {:?}", _i, shards[_i]);
-        _i = _i + 1;
+    //let mut _i = 0;
+    //for shard in shards.iter(){
+    for _r in 0..shards.len(){    
+        println!("shard {} of shards = {:?}", _r, shards[_r]);
     }
 
     // Convert back to normal shard arrangement
